3a5f0f588eb129f51b62b9554b98a3dd0c759f7c
Miles Young <mwyoung2@gmail.com>
2019-09-07 17:53:47 -0700

    Added PIR motion and button test

---
+++ tests/Motion/Motion.ino
@@ -0,0 +1,55 @@
+//for testing the button and pir motion sensor
+#define pirMotion D2
+#define button D1
+
+bool pirLast = LOW;
+bool pirInput = LOW;
+bool buttonLast = LOW;
+bool buttonInput = LOW;
+uint8_t count = 0;
+
+void printBool(bool in);
+
+void setupLED(){
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void setup(void) {
+    Serial.begin(115200);
+    setupLED();
+    pinMode(pirMotion, INPUT);
+    pinMode(button, INPUT);
+    Serial.println();
+}
+
+void loop(){
+    pirInput = digitalRead(pirMotion);
+    digitalWrite(LED_BUILTIN, !pirInput);
+    if ((pirInput == HIGH) && (pirLast == LOW)){
+        Serial.print(".");
+        count++;
+    }
+    pirLast = pirInput; //get previous
+
+    buttonInput = digitalRead(button);
+    if ((buttonInput == HIGH) && (buttonLast == LOW)){
+        Serial.print("#");
+        count++;
+    }
+    buttonLast = buttonInput;
+
+    delay(10);
+    if (count > 25){
+        Serial.println();
+    }
+}
+
+void printBool(bool in){
+    if (in){
+        Serial.print("T");
+    }
+    else {
+        Serial.print("F");
+    }
+}
